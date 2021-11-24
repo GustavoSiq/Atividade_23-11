d0f2320550cc99b8c002c0c926b5d6d52964a7d8
Gustavo Siqueira <gustavosiqueira.97@gmail.com>
2021-11-23 22:16:04 -0300

    first commit

---
+++ exer_23-11.py
@@ -0,0 +1,68 @@
+# Faça um programa que peça um número e então mostre da seguinte forma:
+# "O número informado foi xx"
+
+# numero = input("Por favor, informe um número: ")
+
+# print(f"O número informado foi o {numero}")
+
+# Faça um programa que pergunte a idade do usuário e informe se ele está apto a votar ou não caso a idade seja entre 18 e 70 anos.
+
+# resp = "S"
+# while resp == "S" or resp == "s":
+#     idade = int(input("Informe sua idade: "))
+
+#     if idade >= 0 and idade <16:
+#         print("Você ainda não pode votar")
+#     elif idade >= 16 and idade <18 or idade > 70:
+#         print("Pode votar, mas não é obrigatório!")
+#     elif idade >= 18 and idade <=70:
+#         print("Voto obrigatório!")
+#     else:
+#         print("Idade inválida...")
+
+#     resp = input("Deseja adicionar nova idade(S ou N)? ")
+
+# Faça um programa, utilizando while, que mostre na tela os números de 0 a 100.
+
+# cont = 0
+
+# while cont <= 100:
+#     print(cont)
+#     cont += 1
+
+# Faça um programa, utilizando while e listas, que permita o usuário escrever o nome de cinco pessoas e os mostre na tela.
+
+# cont = 1
+# lista = []
+
+# while cont <= 5:
+#     pessoa = input("Digite o nome de uma pessoa: ")
+#     lista.append(pessoa)
+#     cont += 1
+
+# print(lista)
+
+# Faça um Programa que leia três números inteiros, em seguida mostre o maior e o menor deles.
+
+# n1 = int(input("Digite o primeiro valor: "))
+# n2 = int(input("Digite o segundo valor: "))
+# n3 = int(input("Digite o terceiro valor: "))
+# maior = n1
+# menor = n1
+
+# # maior
+# if n2 > maior:
+#     maior = n2
+# if n3 > maior:
+#     maior = n3
+
+# print(f"O maior número é o {maior}")
+
+# # menor
+# if n2 < menor:
+#     menor = n2
+# if n3 < menor:
+#     menor = n3
+
+# print(f"O menor número é o {menor}")
+    
